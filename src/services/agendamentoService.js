530187ab109cce21df27defd6ed9c68be44eb862
GustavoVFA-SPTECH <gustavo.famorim@sptech.school>
2026-04-25 23:04:29 -0300

    fix: agendamentoService

--- src/services/agendamentoService.js
+++ src/services/agendamentoService.js
@@ -1,8 +1,24 @@
 import { api } from "./api.js";
 
 export const agendamentoService = {
+
   async getKpis() {
     const response = await api.get("/consultas/kpisDashboardAgendamentos");
+    return response.data;
+  },
+
+  async getKpiCancelamentos() {
+    const response = await api.get("/consultas/kpiCancelamentos");
+    return response.data;
+  },
+
+  async getQtdConsultasHoje() {
+    const response = await api.get("/consultas/qtdConsultasHoje");
+    return response.data;
+  },
+
+  async getQtdConsultasMesAtual() {
+    const response = await api.get("/consultas/qtdConsultasMesAtual");
     return response.data;
   },
 
@@ -16,11 +32,49 @@ export const agendamentoService = {
     return response.data;
   },
 
-  async getGraficoAvaliacaoFuncionarios() {
-    return [];
+  async getGraficoConsultasMes() {
+    const response = await api.get("/consultas/graficoConsultasMes");
+    return response.data;
   },
 
-  async getGraficoAvaliacaoConsultas() {
-    return [];
+  async getGraficoFaturamentoMensal() {
+    const response = await api.get("/consultas/graficoFaturamentoMensal");
+    return response.data;
+  },
+
+  async getResumoFinanceiro() {
+    const response = await api.get("/consultas/resumoFinanceiro");
+    return response.data;
+  },
+
+  async getGraficoComparacaoCustoReceita() {
+    const response = await api.get("/consultas/graficoComparacaoCusto");
+    return response.data;
+  },
+
+  async getQtdConsultasFuncionario(idFuncionario) {
+    const response = await api.get(
+      `/consultas/qtdFuncionarioConsultas/${idFuncionario}`
+    );
+    return response.data;
+  },
+
+  async getQtdConsultasPaciente(idPaciente) {
+    const response = await api.get(
+      `/consultas/qtdPacienteConsultas/${idPaciente}`
+    );
+    return response.data;
+  },
+
+  async getConsultasHoje() {
+    const response = await api.get("/consultas/filtrarConsultasHoje");
+    return response.data;
+  },
+
+  async getConsultasHojePorFuncionario(idFuncionario) {
+    const response = await api.get(
+      `/consultas/filtrarConsultasHoje/${idFuncionario}`
+    );
+    return response.data;
   },
 };

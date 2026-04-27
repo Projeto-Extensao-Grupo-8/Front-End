33d4a7aef8fb0e525828f7acdcb53918a15b9e92
Robert1730 <robert.souza@sptech.school>
2026-04-27 20:14:55 -0300

    Alterações Nginx

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://54.236.10.100:8080",
 });
 
 api.interceptors.request.use((config) => {
